bde973da105e83d748125673f4fb86f8b726fdee
Harshvardhan Gandharv <hvardhan.1602@gmail.com>
2025-07-22 16:36:46 +0530

    jsx commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Success from './pages/Success.jsx';
-import Error from './pages/error.jsx';
+import Error from './pages/Error.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 const App = () => {
